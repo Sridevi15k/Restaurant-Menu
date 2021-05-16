115d3c52e28b777b6e814b8b24a4f62cc1a9a8b2
Sridevi15k <sridevi15k@gmail.com>
2021-05-16 09:54:33 -0500

    Restaurant Menu to Restaurant-Menu

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,17 @@
+package restaurant;
+
+import java.lang.reflect.Array;
+
+public class Menu {
+    private MenuItem[] menuItems;
+
+    public static void main(String[] args) {
+
+    }
+
+    public Menu(MenuItem[]menuItems){
+        MenuItem item = new MenuItem();
+        this.menuItems = menuItems;
+        }
+
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,12 @@
+package restaurant;
+
+import java.util.Date;
+
+public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private boolean newItem;
+    private Date updatedItem;
+}
